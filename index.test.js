40164ff36172ff6af632006978d19ab31870adf1
RIKU <rank228130@gmail.com>
2021-08-01 21:54:53 +0900

    feat: describe
    to contain same function

--- index.test.js
+++ index.test.js
@@ -1,34 +1,40 @@
 import { addString, toNumber, filterOrange } from "./index";
 
 //  lesson01
-test("aとbが'a'、'b'の時'ab'となる", () => {
-  expect(addString("a", "b")).toBe("ab");
-});
+describe("addString", () => {
+  test("a and b are 'a' , 'b', to equal 'ab'", () => {
+    expect(addString("a", "b")).toBe("ab");
+  });
 
-test("aが'a', bが1の時'a1'が返る", () => {
-  expect(addString("a", 1)).toBe("a1");
-});
+  test("a is 'a' , b is 1, to equal 'a1'", () => {
+    expect(addString("a", 1)).toBe("a1");
+  });
 
-test("aが1、bが1の時-1が返る", () => {
-  expect(addString(1, 1)).toBe(-1);
+  test("a is 1 , b is 1, to equal -1", () => {
+    expect(addString(1, 1)).toBe(-1);
+  });
 });
 
 //  lesson02
-test("str が '123'だった場合、123が返る", () => {
-  expect(toNumber("123", 10)).toBe(123);
+describe("toNumber", () => {
+  test("str is '123', to equal 123", () => {
+    expect(toNumber("123", 10)).toBe(123);
+  });
+
+  test("str is '一二三', to equal NaN", () => {
+    expect(toNumber("一二三", 10)).toBe(NaN);
+  });
 });
 
-test("str が '一二三'だった場合、NaNが返る", () => {
-  expect(toNumber("一二三", 10)).toBe(NaN);
-});
+describe("filterOrange", () => {
+  //  lesson03
+  test("arr contains 'orange', return a new array with only 'orange' in it", () => {
+    const fruit = ["orange", "apple", "peach"];
+    expect(filterOrange(fruit)).toEqual(["orange"]);
+  });
 
-//  lesson03
-test("arrが'orange'を含んでいる場合、'orange'のみが格納された新しい配列を返す", () => {
-  const fruit = ["orange", "apple", "peach"];
-  expect(filterOrange(fruit)).toEqual(["orange"]);
-});
-
-test("arrが'orange'を含んでいない場合、空の配列が返る", () => {
-  const fruit = ["apple", "peach"];
-  expect(filterOrange(fruit)).toEqual([]);
+  test("arr does not contain 'orange', an empty array", () => {
+    const fruit = ["apple", "peach"];
+    expect(filterOrange(fruit)).toEqual([]);
+  });
 });
